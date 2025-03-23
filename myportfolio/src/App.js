1a9ba39b81ed67cd97f83beb6b976bae90e20849
gioveapp <giove01@proton.me>
2025-03-23 19:39:02 +0000

    test run 1

---
+++ myportfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a placeholder homepage. Stay tuned for awesome projects!</p>
+    </div>
+  );
+}
+
+export default App;
+
